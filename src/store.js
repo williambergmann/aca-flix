71cd1c5ceee2a99d0874a1ec67f10b4d65ca69c0
williambergmann <williebergmann@gmail.com>
2019-06-06 20:41:01 -0500

    redux thunk and Provider wrapper added

--- src/store.js
+++ src/store.js
@@ -1,6 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import state from "./state";
 import reducers from "./reducers";
 //Use redux-thunk middleware
+import thunk from 'redux-thunk'
 
-export default createStore(reducers, state);
+
+export default createStore(reducers, state, applyMiddleware(thunk));
